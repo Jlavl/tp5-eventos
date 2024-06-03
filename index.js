e1c363e8e3a356ef7fca6ee389c0d7f48bb313de
Julian <jelaverallauro@gmail.com>
2024-06-03 10:41:29 -0300

    eee

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ entity.id_user =
 ValidacionesHelper.getIntegerPositiveZeroOrDefault(req.user.id,0);
 
 */
+import  Express  from "express";
+import cors from "cors";
+
 import config from "./src/configs/db.config";
 import pkg from 'pg';
+
+const app = express();
+const port = 5432;
 const { Client} = pkg;
 
 // https://node-postgres.com/apis/client
@@ -22,3 +28,7 @@ let result = await client.query(sql);
 await client.end();
 // 'rows' es un array. rows[0] es el 1°er registro.
 console.log(result.rows)
+
+app.use(cors());
+app.use(express.json);
+//app.use('/front', express.static('public'));
